284a91afc5ff6789505531d4af6ff258bb22c9e6
Francisco Ngonde Afonso <157767150+afonsowebdev@users.noreply.github.com>
2025-08-19 12:19:28 +0100

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let video = document.querySelector(".footer-video");
+    if (video) {
+        video.playbackRate = 0.9; // Reduz a velocidade para 70% da velocidade normal
+    }
+});
+
+
+
